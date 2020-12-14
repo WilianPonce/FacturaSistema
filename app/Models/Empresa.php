d6814ffeea31b3a581a8079bb28cd89b0e48a5c9
WilianPonce <wily2809@hotmail.com>
2020-12-13 23:40:27 -0500

    cambios de pruebas allowed

--- app/Models/Empresa.php
+++ app/Models/Empresa.php
@@ -11,6 +11,7 @@ class Empresa extends Model
 
     public $allowed = ['periodo', 'razon_social', 'razon_comercial', 'ruc', 'direccion', 'telefono', 'email', 'email_password', 'email_server', 'email_port', 'email_security', 'obligado_contabilidad', 'contador_nombre', 'contador_ruc', 'representante_nombre', 'representante_ruc', 'periodo_inicio', 'periodo_fin', 'tipo_cuentas', 'logotipo', 'firma_electronica', 'firma_electronica_password', 'firma_electronica_expiracion', 'firma_electronica_actualizacion', 'tipo_emision', 'ambiente', 'facturar_negativo', 'moneda_id', 'provincia_id', 'ciudad_id', 'email_facturacion', 'leyenda_facturacion', 'created_at', 'updated_at'];
 
+
     protected $guarded = [];
 
     protected $casts = [
